522c3f521193be6a569da66fa0325d96e938df24
JulakantiManith <147398979+JulakantiManith@users.noreply.github.com>
2024-11-04 14:38:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ app.include_router(auth.router, prefix='/auth')
 app.include_router(song.router, prefix='/song')
 
 Base.metadata.create_all(engine)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
